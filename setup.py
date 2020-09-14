6321123f7c753be1540e998b55ea4ff62dc37ef4
kannan-Balakrishnan <kannankanna7196@gmail.com>
2020-09-15 01:15:57 +0530

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='We are creating a covid -19 Data analysis prototype for better visualization of data.',
+    author='Kannan',
+    license='',
+)
